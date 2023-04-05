4d955f0739fdc32c3686f7817ea13140fa628e7c
Ella Kempel <ella.lernt@mail.de>
2023-04-05 15:39:58 +0200

    delete tests

--- components/Clue/Clue.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Clue from ".";
-
-test("renders clue", () => {
-  render(<Clue />);
-  const clueText = screen.getByRole(/Gib den Code ein!/i);
-  expect(clueText).toBeInTheDocument();
-});
