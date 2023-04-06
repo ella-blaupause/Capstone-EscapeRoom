485bc415910e6431814fc9931abf78efa6ce35a2
Ella Kempel <ella.lernt@mail.de>
2023-04-06 14:50:54 +0200

    add on/off button

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,75 @@
+import useLocalStorageState from "use-local-storage-state";
 import GlobalStyle from "../styles";
 
+const colors = [
+  "yellow",
+  "green",
+  "lightblue",
+  "brown",
+  "red",
+  "gray",
+  "orange",
+  "blueviolet",
+];
+
+const symbols = ["△", "☆", "◇", "❀", "☀︎", "✧"];
+
+function getRandomNumbers(arrayLength) {
+  const numbers = [];
+
+  // Generiert drei zufällige Zahlen, die nur einmal vorkommen
+  while (numbers.length < 3) {
+    const random = Math.floor(Math.random() * arrayLength);
+    if (!numbers.includes(random)) {
+      numbers.push(random);
+    }
+  }
+
+  return numbers;
+}
+const randomNumbers = getRandomNumbers(symbols.length);
+
 export default function App({ Component, pageProps }) {
+  const [randomColor, setRandomColor] = useLocalStorageState("randomColor", {
+    defaultValue: [
+      colors[[Math.floor(Math.random() * symbols.length)]],
+      colors[[Math.floor(Math.random() * symbols.length)]],
+      colors[[Math.floor(Math.random() * symbols.length)]],
+    ],
+  });
+  const [randomSymbol, setRandomSymbol] = useLocalStorageState("randomSymbol", {
+    defaultValue: [
+      symbols[randomNumbers[0]],
+      symbols[randomNumbers[1]],
+      symbols[randomNumbers[2]],
+    ],
+  });
+
+  function getRandomCode() {
+    const randomNumbers = getRandomNumbers(symbols.length);
+
+    setRandomSymbol([
+      symbols[[randomNumbers[0]]],
+      symbols[[randomNumbers[1]]],
+      symbols[[randomNumbers[2]]],
+    ]);
+    setRandomColor([
+      colors[Math.floor(Math.random() * colors.length)],
+      colors[Math.floor(Math.random() * colors.length)],
+      colors[Math.floor(Math.random() * colors.length)],
+    ]);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        getRandomCode={getRandomCode}
+        colors={colors}
+        randomColor={randomColor}
+        randomSymbol={randomSymbol}
+        {...pageProps}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,69 @@
 import Link from "next/link";
 import styled from "styled-components";
+import Clue from "../components/Clue";
+import { useEffect, useState } from "react";
 
+const StyledLayout = styled.div`
+  background-color: ${({ isOn }) => {
+    isOn ? "white" : "lightgray";
+  }};
+  width: 300px;
+  height: 100vw;
+`;
+
+/* filter: ${({ isOn }) => {
+  isOn ? "none" : "brightness(70%)";
+}}; */
+const RoomDiv = styled.div`
+  display: relativ;
+
+  width: 300px;
+  height: 100vw;
+`;
 const DoorButton = styled.button`
   width: 100px;
   height: 150px;
   position: absolute;
-  transform: translate(0,-50%);
-  right: 20%;
-  top 50%;
-  font-size: 80px
+  transform: translate(0, -50%);
+  right: 100px;
+  top: 50%;
+  font-size: 80px;
 `;
 
-const RoomDiv = styled.div`
-  display: relativ;
-  width: 300px;
+const LightButton = styled.button`
+  position: absolute;
+  transform: translate(0, -50%);
+  right: 200px;
+  top: 50%;
 `;
 
-export default function Room() {
+export default function Room({ randomColor, randomSymbol }) {
+  const [isOn, setIsOn] = useState(true);
+  // Componente wir nur beim zweiten Mal gerendert
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  function toggleOnOff() {
+    setIsOn(!isOn);
+  }
+
   return (
-    <RoomDiv>
-      <Link href={"/door"}>
-        <DoorButton>🚪</DoorButton>
-      </Link>
-    </RoomDiv>
+    <>
+      {isClient && (
+        <StyledLayout isOn={isOn}>
+          <RoomDiv>
+            <Clue randomColor={randomColor} randomSymbol={randomSymbol} />
+            <Link href={"/door"}>
+              <DoorButton>🚪</DoorButton>
+            </Link>
+            <LightButton type="button" onClick={toggleOnOff}>
+              on/off
+            </LightButton>
+          </RoomDiv>
+        </StyledLayout>
+      )}
+    </>
   );
 }
