5e4db1772c10d7a61e59b250692006c939c0d3f9
Ella Kempel <ella.lernt@mail.de>
2023-04-09 12:41:50 +0200

    add grid style

--- components/Clue/index.js
+++ components/Clue/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const StyledClue = styled.div`
+  grid-column: 3 / span 6;
+  grid-row: 5 / span 2;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -17,19 +19,16 @@ const StyledColorDiv = styled.div`
 
 export default function Clue({ randomColor, randomSymbol }) {
   return (
-    <>
-      <p>Gib den Code ein!</p>
-      <StyledClue>
-        <StyledColorDiv color={randomColor[0]} data-testid="color-div">
-          {randomSymbol[0]}
-        </StyledColorDiv>
-        <StyledColorDiv color={randomColor[1]} data-testid="color-div">
-          {randomSymbol[1]}
-        </StyledColorDiv>
-        <StyledColorDiv color={randomColor[2]} data-testid="color-div">
-          {randomSymbol[2]}
-        </StyledColorDiv>
-      </StyledClue>
-    </>
+    <StyledClue>
+      <StyledColorDiv color={randomColor[0]} data-testid="color-div">
+        {randomSymbol[0]}
+      </StyledColorDiv>
+      <StyledColorDiv color={randomColor[1]} data-testid="color-div">
+        {randomSymbol[1]}
+      </StyledColorDiv>
+      <StyledColorDiv color={randomColor[2]} data-testid="color-div">
+        {randomSymbol[2]}
+      </StyledColorDiv>
+    </StyledClue>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Clue from "../components/Clue";
-import { useEffect, useState } from "react";
-import useLocalStorageState from "use-local-storage-state";
 
-const StyledLayout = styled.div`
+const GridContainer = styled.div`
   background-color: ghostwhite;
   border: solid ghostwhite;
   filter: ${(props) => (props.isOn ? "brightness(100%)" : "brightness(75%)")};
+  display: grid;
+  grid-template-columns: repeat(15, 1fr);
+  grid-template-rows: repeat(20, 1fr);
   width: 375px;
   height: 667px;
 `;
 
-const DoorButton = styled.button`
+/* const DoorButton = styled.button`
   width: 100spx;
   height: 150px;
   position: absolute;
@@ -23,13 +24,20 @@ const DoorButton = styled.button`
   background: none;
   border: none;
   padding: 0;
+`; */
+
+const DoorButton = styled(Link)`
+  font-size: 100px;
+  grid-column: 12 / span 2;
+  grid-row: 10 / span 3;
+  z-index: -1;
+  text-decoration: none;
+  cursor: default;
 `;
 
 const LightButton = styled.button`
-  position: absolute;
-  transform: translate(0, -50%);
-  right: 100px;
-  top: 50%;
+  grid-column: 11;
+  grid-row: 11;
 `;
 
 export default function Room({
@@ -39,18 +47,14 @@ export default function Room({
   isOn,
 }) {
   return (
-    <>
-      <StyledLayout isOn={isOn}>
-        {!isOn && (
-          <Clue randomColor={randomColor} randomSymbol={randomSymbol} />
-        )}
-        <Link href={"/door"}>
-          <DoorButton>🚪</DoorButton>
-        </Link>
-        <LightButton type="button" onClick={onToggleOnOff}>
-          on/off
-        </LightButton>
-      </StyledLayout>
-    </>
+    <GridContainer isOn={isOn}>
+      {!isOn && <Clue randomColor={randomColor} randomSymbol={randomSymbol} />}
+
+      <DoorButton href={"/door"}>🚪</DoorButton>
+
+      <LightButton type="button" onClick={onToggleOnOff}>
+        on/off
+      </LightButton>
+    </GridContainer>
   );
 }
