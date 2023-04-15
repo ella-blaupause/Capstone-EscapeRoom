7c60697bb3f9a8f51a9c8794dd0f02110294d48b
Ella Kempel <ella.lernt@mail.de>
2023-04-15 17:32:06 +0200

    add form

--- components/CrosswordLayout/index.js
+++ components/CrosswordLayout/index.js
@@ -24,54 +24,134 @@ const StyledOl = styled.ol`
   grid-column: 2;
   grid-row: 1;
 `;
+const StyledForm = styled.form`
+  grid-column: 1 / span 2;
+  grid-row: 2;
+`;
 
 export default function CrosswordLayout() {
+  const [currentClueId, setCurrentClueId] = useState(null);
+  const [crosswordClues, setCrosswordClues] = useState(initialCrosswordClues);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    checkAnswer(data);
+  }
+  function handleClick(crosswordCluesId) {
+    setCurrentClueId(crosswordCluesId);
+  }
+  function checkAnswer(data) {
+    if (currentClueId === null) {
+      alert("Wähle eine Frage aus!");
+      return;
+    }
+    const currentCrosswordClue = crosswordClues.find(
+      (clue) => clue.id === currentClueId
+    );
+    const correctAnswer = currentCrosswordClue.answer.toLowerCase();
+    const enteredAnswer = data.answer.toLowerCase();
+
+    if (enteredAnswer === correctAnswer) {
+      console.log("richtig");
+      setCrosswordClues((clues) =>
+        clues.map((clue) =>
+          clue.id === currentClueId
+            ? { ...clue, isCorrectlyAnswered: true }
+            : clue
+        )
+      );
+    }
+  }
+
   return (
     <>
       <StyledTable>
         <tbody>
           <Tr>
             <Td isBlack></Td>
-            <Td></Td>
+            <Td>
+              <sup>1</sup>
+              {crosswordClues[0].isCorrectlyAnswered ? "M" : null}
+            </Td>
             <Td isBlack></Td>
             <Td isBlack></Td>
             <Td isBlack></Td>
           </Tr>
           <Tr>
             <Td isBlack></Td>
-            <Td></Td>
-            <Td></Td>
-            <Td></Td>
+            <Td>
+              <sup>2</sup>
+              {crosswordClues[0].isCorrectlyAnswered ||
+              crosswordClues[1].isCorrectlyAnswered
+                ? "U"
+                : null}
+            </Td>
+            <Td>{crosswordClues[1].isCorrectlyAnswered ? "H" : null}</Td>
+            <Td>{crosswordClues[1].isCorrectlyAnswered ? "R" : null}</Td>
             <Td isBlack></Td>
           </Tr>
           <Tr>
             <Td isBlack></Td>
-            <Td></Td>
+            <Td>{crosswordClues[0].isCorrectlyAnswered ? "S" : null}</Td>
             <Td isBlack></Td>
             <Td isBlack></Td>
-            <Td></Td>
+            <Td>
+              <sup>4</sup>
+              {crosswordClues[3].isCorrectlyAnswered ? "O" : null}
+            </Td>
           </Tr>
           <Tr>
-            <Td></Td>
-            <Td></Td>
-            <Td></Td>
-            <Td></Td>
-            <Td></Td>
+            <Td>
+              <sup>3</sup>
+              {crosswordClues[2].isCorrectlyAnswered ? "T" : null}
+            </Td>
+            <Td>
+              {crosswordClues[0].isCorrectlyAnswered ||
+              crosswordClues[2].isCorrectlyAnswered
+                ? "I"
+                : null}
+            </Td>
+            <Td>{crosswordClues[2].isCorrectlyAnswered ? "S" : null}</Td>
+            <Td>{crosswordClues[2].isCorrectlyAnswered ? "C" : null}</Td>
+            <Td>
+              {crosswordClues[2].isCorrectlyAnswered ||
+              crosswordClues[3].isCorrectlyAnswered
+                ? "H"
+                : null}
+            </Td>
           </Tr>
           <Tr>
             <Td isBlack></Td>
-            <Td></Td>
+            <Td>{crosswordClues[0].isCorrectlyAnswered ? "K" : null}</Td>
             <Td isBlack></Td>
             <Td isBlack></Td>
-            <Td></Td>
+            <Td>{crosswordClues[3].isCorrectlyAnswered ? "R" : null}</Td>
           </Tr>
         </tbody>
       </StyledTable>
       <StyledOl>
         {crosswordClues.map((crosswordClue) => (
-          <li key={crosswordClue.id}>{crosswordClue.question}</li>
+          <li
+            key={crosswordClue.id}
+            onClick={() => handleClick(crosswordClue.id)}
+          >
+            {crosswordClue.question}
+          </li>
         ))}
       </StyledOl>
+      <StyledForm onSubmit={handleSubmit}>
+        <legend>Hinweis:</legend>
+        <label htmlFor="answer">
+          {currentClueId
+            ? crosswordClues[currentClueId - 1].question
+            : "Wähle eine Frage aus"}
+        </label>
+        <br />
+        <input id="answer" type="text" name="answer" />
+      </StyledForm>{" "}
     </>
   );
 }
