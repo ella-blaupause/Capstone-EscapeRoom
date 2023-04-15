3baa64d5deb786ab6bf8dc7fd237a6120b2fab9f
Ella Kempel <ella.lernt@mail.de>
2023-04-15 21:34:03 +0200

    fix tests

--- components/EntryForm/EntryForm.test.js
+++ components/EntryForm/EntryForm.test.js
@@ -5,15 +5,19 @@ test("renders a label and an input with the correct attributes", () => {
   const onData = jest.fn();
   const currentClueId = 1;
 
-  render(<EntryForm onData={onData} currentClueId={currentClueId} />);
+  render(
+    <EntryForm
+      onData={onData}
+      currentClueId={currentClueId}
+      name="answer"
+      labelText="Was wird in Konzerten, Opern, Musicals und Filmen aufgeführt?"
+      placeholder="Antwort eingeben"
+    />
+  );
 
-  const labelElement = screen.getByLabelText(/Hinweis:/i);
-  const inputElement = screen.getByPlaceholderText("Antwort eingeben");
-
-  expect(labelElement).toBeInTheDocument();
-  expect(labelElement.getAttribute("for")).toBe("answer");
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement.getAttribute("id")).toBe("answer");
-  expect(inputElement.getAttribute("type")).toBe("text");
-  expect(inputElement.getAttribute("name")).toBe("answer");
+  const input = screen.getByLabelText(
+    /Was wird in Konzerten, Opern, Musicals und Filmen aufgeführt?/i
+  );
+  expect(input).toHaveAttribute("placeholder", "Antwort eingeben");
+  expect(input).toHaveAttribute("name", "answer");
 });
