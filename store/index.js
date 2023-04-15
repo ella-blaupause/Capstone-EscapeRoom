172333bc6a48242fdae6294a86f5eed942b2138d
Ella Kempel <ella.lernt@mail.de>
2023-04-15 15:34:48 +0200

    create incomplete store

--- store/index.js
+++ store/index.js
@@ -1,1 +1,29 @@
 import create from "zustand";
+import {
+  colors,
+  getRandomColor,
+  initialPuzzlePieces,
+  symbols,
+} from "../utils/utils";
+
+const randomNumbers = getRandomNumbers(symbols.length);
+export default useStore = create((set) => ({
+  isOn: true,
+  randomColors: [
+    colors[getRandomColor()],
+    colors[getRandomColor()],
+    colors[getRandomColor()],
+  ],
+  randomSymbols: [
+    symbols[randomNumbers[0]],
+    symbols[randomNumbers[1]],
+    symbols[randomNumbers[2]],
+  ],
+  puzzlePieces: initialPuzzlePieces,
+  countPieces: 0,
+  switchLigth: () => set((state) => ({ isOn: !state.isOn })),
+  handleCollect: (puzzlePieceId) =>
+    set((state) => ({
+      countPieces: state.countPieces + 1,
+    })),
+}));
