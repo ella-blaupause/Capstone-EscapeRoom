09a0ba581eb4b9cd7a55b61bcd8839c90f291a97
Ella Kempel <ella.lernt@mail.de>
2023-04-19 17:59:41 +0200

    fix test

--- components/Header/Header.test.js
+++ components/Header/Header.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import Header from "./Header";
+import Header from ".";
 
 test("renders the title correctly", async () => {
   const title = "Test Title";
-  render(<Header>{title}</Header>);
-  const titleElement = await screen.findByText(title);
+  render(<Header isBackArrow={false}>{title}</Header>);
+  const titleElement = await screen.findByText(/Test Title/i);
   expect(titleElement).toBeInTheDocument();
 });
