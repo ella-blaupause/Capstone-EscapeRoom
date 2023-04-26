2497652d3bfcb59364fbbfa12e0daad398487989
Ella Kempel <ella.lernt@mail.de>
2023-04-26 11:29:51 +0200

    add html-select

--- components/SelectAvatar/index.js
+++ components/SelectAvatar/index.js
@@ -1,18 +1,40 @@
 import Image from "next/image";
 import { avatars } from "../../utils/utils";
+import { useState } from "react";
 
 export default function SelectAvatar() {
+  const [selectedAvatar, setSelectedAvatar] = useState(avatars[0]);
+  function handleAvatarChange(event) {
+    event.preventDefault();
+    const selectedAvatarId = parseInt(event.target.value);
+    const selectedAvatar = avatars.find(
+      (avatar) => avatar.id === selectedAvatarId
+    );
+
+    setSelectedAvatar(selectedAvatar);
+  }
+
   return (
     <>
       <form>
-        <label htmlFor="avatars">Avatar</label>
-        <select name="avatars" id="avatars">
-          <option value={"avatar-1"}>
-            <Image src={avatars[0].src} width={12} height={12} alt="avatar" />
-          </option>
+        <label htmlFor="avatars">Avatar: </label>
+        <select name="avatars" id="avatars" onChange={handleAvatarChange}>
+          {avatars.map((avatar) => {
+            return (
+              <option key={avatar.id} value={avatars.id}>
+                {avatar.id}
+              </option>
+            );
+          })}
         </select>
       </form>
-      <Image src={avatars[0].src} width={120} height={120} alt="avatar" />
+      <Image
+        priority
+        src={selectedAvatar.src}
+        width={120}
+        height={120}
+        alt="avatar"
+      />
     </>
   );
 }
